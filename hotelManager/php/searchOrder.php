fb7b10e0006545bcc371e4aaa1c43a73dee00485
sjw1993 <981844871@qq.com>
2017-04-15 14:23:38 +0800

    one commit

---
+++ hotelManager/php/searchOrder.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$mysqli = new mysqli('localhost','root','root','db_hotel');
+	
+	$sql = 'select * from orderroom';
+	
+	$data = $mysqli->query($sql);
+	
+	$aOrder = array();
+	
+	while($oOder = $data->fetch_assoc()){
+		array_push($aOrder,$oOder);
+	}
+	echo json_encode($aOrder);
+?>
